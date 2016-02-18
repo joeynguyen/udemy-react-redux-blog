a6156ddebe9020a9cf7633b180b1551f1fb17c34
Joey Nguyen <jnn193@gmail.com>
2016-02-18 01:12:45 -0600

    Integrated redux-form into project to managing forms. Created form to create new blog posts by submitting to API.

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
   render() {
+    // handleSubmit and fields are from redux-form
+    // const title = this.props.fields.title;
+    // const handleSubmit = this.props.handleSubmit;
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+
     return (
-      <div>Create form</div>
+      <form onSubmit={handleSubmit(this.props.doCreatePost)}>
+        <h3>Create a New Post</h3>
+
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" {...title} />
+        </div>
+
+        <div className="form-group">
+          <label>Category</label>
+          <input type="text" className="form-control" {...categories} />
+        </div>
+
+        <div className="form-group">
+          <label>Content</label>
+          <textarea className="form-control" {...content} />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+// connect: 1st argument is mapStateToProps, 2nd state is mapDispatchToProps
+// reduxForm: 1st argument is form config, 2nd is mapStateToProps, 3rd state is mapDispatchToProps
+
+export default reduxForm({
+  form: 'PostNewForm',
+  fields: ['title', 'categories', 'content']
+}, null, { doCreatePost: createPost })(PostsNew);
