ed2c0512e0ee25363d30df125b3c6298fdf30942
Joey Nguyen <jnn193@gmail.com>
2016-02-18 01:42:13 -0600

    Added additional error validation for other fields and added red color to fields with errors.

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -13,7 +13,7 @@ class PostsNew extends Component {
       <form onSubmit={handleSubmit(this.props.doCreatePost)}>
         <h3>Create a New Post</h3>
 
-        <div className="form-group">
+        <div className={`form-group ${(title.touched && title.invalid) ? 'has-danger' : ''}`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title} />
           <div className="text-help">
@@ -21,14 +21,20 @@ class PostsNew extends Component {
           </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${(categories.touched && categories.invalid) ? 'has-danger' : ''}`}>
           <label>Category</label>
           <input type="text" className="form-control" {...categories} />
+          <div className="text-help">
+            {categories.touched ? categories.error : ''}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${(content.touched && content.invalid) ? 'has-danger' : ''}`}>
           <label>Content</label>
           <textarea className="form-control" {...content} />
+          <div className="text-help">
+            {content.touched ? content.error : ''}
+          </div>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
@@ -42,6 +48,12 @@ function validateForm(values) {
 
   if (!values.title) {
     errors.title = 'Enter a title';
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter categories';
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content';
   }
 
   return errors;
