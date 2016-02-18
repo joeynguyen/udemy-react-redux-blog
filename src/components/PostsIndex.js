59fbffe968078f34dba7c22d36dcba25fe01ddaa
Joey Nguyen <jnn193@gmail.com>
2016-02-18 00:11:04 -0600

    Created actions and reducers to fetch our data from the ReduxBlog Heroku app API.

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-  return (
-    <div>List of blog posts</div>
-  );
-};
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.doFetchPosts();
+  }
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
+}
+
+export default connect(null, { doFetchPosts: fetchPosts })(PostsIndex);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import promise from 'redux-promise';
 import reducers from './reducers';
 import routes from './routes';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
