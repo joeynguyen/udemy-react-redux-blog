cf883655c161f5e908bc3143b8d84f5feece171e
Joey Nguyen <jnn193@gmail.com>
2016-02-18 01:47:02 -0600

    Added cancel button on form page which changes route back to home page.

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { Link } from 'react-router';
 import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
@@ -38,6 +39,7 @@ class PostsNew extends Component {
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
