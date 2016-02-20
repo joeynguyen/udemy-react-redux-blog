a89cce63c011f16271e2b119120aa0d679358833
Joey Nguyen <jnn193@gmail.com>
2016-02-20 15:59:55 -0600

    Added functionality to fetch individual posts for the PostShow route and display them on the page.

--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
 
 class PostShow extends Component {
+  componentWillMount() {
+    this.props.doFetchPost(this.props.params.id);
+  }
   render() {
+    const { post } = this.props;
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+    console.log(post);
     return (
-      <div>Show post {this.props.params.id}</div>
+      <div>
+        <h3>{post.title}</h3>
+        <h6>{post.categories}</h6>
+        <p>{this.props.post.content}</p>
+      </div>
     );
   }
 }
 
-export default PostShow;
+function mapStateToProps(state) {
+  return {
+    post: state.posts.post
+  };
+}
+
+export default connect(mapStateToProps, { doFetchPost: fetchPost} )(PostShow);
